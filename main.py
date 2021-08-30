c065a9ca59b8da8d7d3ce3a2249948fce190575c
Vladislav <vladislavvoin@gmail.com>
2021-08-30 13:07:16 +0300

    ver 0.2
    +get death count

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 '''
-ver 0.1
+ver 0.2
 This is a simple parser for winning characters for Tales of Maj'Eyal 1.7.4
 To change the version of the game you need to look up the patch id on the character vault website
 Range of the page was calculated manually, because vault website is bare-bones and shows only 25 characters per page and
@@ -7,8 +7,7 @@ doesn't show you total amount of characters or pages, will try to find a way to 
 No exploration mode and easy difficulty because of my personal bias :)
 TO-DO
 optimize the counter algorithm
-find a way to calculate amount of pages
-add a function for calculating a total amount of characters for the diff+mode
+find a way to calculate amount of pages (check a page for next button if no button stop)
 '''
 
 import requests
@@ -26,7 +25,7 @@ def get_winners(mode='roguelike', diff='normal'):
     tag_diff = {'normal': '6',
                 'nightmare': '26',
                 'insane': '36',
-                'madness': '227', }
+                'madness': '227'}
     for i in range(0, 24):
         URL = 'https://te4.org/characters-vault?tag_name=&tag_level_min=&tag_level_max=&tag_winner=winner&tag_permadeath%5B%5D={0}&tag_difficulty%5B%5D={1}&tag_campaign%5B%5D=2&tag_game%5B%5D=699172&page={2}'.format(
             tag_mode[mode], tag_diff[diff], i)
@@ -41,8 +40,36 @@ def get_winners(mode='roguelike', diff='normal'):
     return winners_list
 
 
-def count_classes(mode='roguelike', diff='normal'):
-    winners_list = get_winners(mode, diff)
+def get_deaths(mode='roguelike', diff='normal'):
+    deaths_list = []
+    tag_mode = {'adventure': '65',
+                'roguelike': '66'}
+    tag_diff = {'normal': '6',
+                'nightmare': '26',
+                'insane': '36',
+                'madness': '227'}
+    for i in range(0, 3):
+        #lvl 15+ deaths, maybe add custom value when range of pages will be automated
+        URL = 'https://te4.org/characters-vault?tag_name=&tag_level_min=15&tag_level_max=&tag_dead=dead&tag_permadeath%5B%5D={0}&tag_difficulty%5B%5D={1}&tag_campaign%5B%5D=2&tag_game%5B%5D=699172&page={2}'.format(
+            tag_mode[mode], tag_diff[diff], i)
+        print(URL)
+        page = requests.get(URL)
+        sleep(randint(1, 3))
+        soup = BeautifulSoup(page.content, "html.parser")
+        char_table = soup.find(id="characters")
+        my_table = char_table.find_all(style="color:#FF0000")
+        for tag in my_table:
+            deaths_list.append(tag.get_text())
+    return deaths_list
+
+
+def count_classes(mode='roguelike', diff='normal', status='win'):
+    if status == 'win':
+        characters_list = get_winners(mode, diff)
+    elif status == 'dead':
+        characters_list = get_deaths(mode, diff)
+    else:
+        return 'Wrong status'
     class_stats = {'Berserker': 0, 'Bulwark': 0, 'Archer': 0, 'Arcane Blade': 0, 'Brawler': 0, 'Rogue': 0,
                    'Shadowblade': 0,
                    'Marauder': 0, 'Skirmisher': 0, 'Alchemist': 0, 'Archmage': 0, 'Necromancer': 0,
@@ -58,17 +85,14 @@ def count_classes(mode='roguelike', diff='normal'):
                   'Paradox Mage', 'Temporal Warden', 'Mindslayer', 'Solipsist', 'Possessor', 'Adventurer',
                   'Wanderer', 'Sawbutcher', 'Gunslinger', 'Psyshot', 'Annihilator', 'Writhing One',
                   'Cultist of Entropy']
-    for winner in winners_list:
-        if winner is not None:
-            winners_class = re.search(r'\s\d+\s\w+\s(.*)', winner)
-            if winners_class is not None:  # for broken non-unicode char sheets
+    for character in characters_list:
+        if character is not None:
+            characters_class = re.search(r'\s\d+\s\w+\s(.*)', character)
+            if characters_class is not None:  # for broken non-unicode char sheets
                 for class_name in class_list:
-                    if class_name in winners_class[1]:
+                    if class_name in characters_class[1]:
                         class_stats[class_name] += 1
                         break
     df = pd.DataFrame.from_dict(class_stats, orient='index')
-    df.to_excel('winners_{0}_{1}.xlsx'.format(mode, diff), header=True)
+    df.to_excel('characters_{0}_{1}_{2}.xlsx'.format(mode, diff, status), header=True)
     return df
-
-
-print(count_classes('roguelike', 'normal'))
