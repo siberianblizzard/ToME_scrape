142538f6da048f0019e294a0da52472d553bc625
Vladislav <vladislavvoin@gmail.com>
2021-08-30 15:09:17 +0300

    Automated page counter, rewrote main function to remove duplicate code, added minimal level parameter

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 '''
-ver 0.2
+ver 0.3
 This is a simple parser for winning characters for Tales of Maj'Eyal 1.7.4
 To change the version of the game you need to look up the patch id on the character vault website
 Range of the page was calculated manually, because vault website is bare-bones and shows only 25 characters per page and
 doesn't show you total amount of characters or pages, will try to find a way to fix it to optimize the script.
 No exploration mode and easy difficulty because of my personal bias :)
-TO-DO
-optimize the counter algorithm
-find a way to calculate amount of pages (check a page for next button if no button stop)
+TODO
+Make a proper pandas data frame from parsing
 '''
 
 import requests
@@ -18,56 +17,47 @@ from random import randint
 from bs4 import BeautifulSoup
 
 
-def get_winners(mode='roguelike', diff='normal'):
-    winners_list = []
+def get_characters(mode: str = 'roguelike', diff: str = 'normal', min_lvl: int = 15, status: str = 'win'):
+    characters_list = []
     tag_mode = {'adventure': '65',
                 'roguelike': '66'}
     tag_diff = {'normal': '6',
                 'nightmare': '26',
                 'insane': '36',
                 'madness': '227'}
-    for i in range(0, 24):
-        URL = 'https://te4.org/characters-vault?tag_name=&tag_level_min=&tag_level_max=&tag_winner=winner&tag_permadeath%5B%5D={0}&tag_difficulty%5B%5D={1}&tag_campaign%5B%5D=2&tag_game%5B%5D=699172&page={2}'.format(
-            tag_mode[mode], tag_diff[diff], i)
-        print(URL)
-        page = requests.get(URL)
-        sleep(randint(1, 3))
-        soup = BeautifulSoup(page.content, "html.parser")
-        char_table = soup.find(id="characters")
-        my_table = char_table.find_all(style="color:#00FF00")
-        for tag in my_table:
-            winners_list.append(tag.get_text())
-    return winners_list
-
-
-def get_deaths(mode='roguelike', diff='normal'):
-    deaths_list = []
-    tag_mode = {'adventure': '65',
-                'roguelike': '66'}
-    tag_diff = {'normal': '6',
-                'nightmare': '26',
-                'insane': '36',
-                'madness': '227'}
-    for i in range(0, 3):
-        #lvl 15+ deaths, maybe add custom value when range of pages will be automated
-        URL = 'https://te4.org/characters-vault?tag_name=&tag_level_min=15&tag_level_max=&tag_dead=dead&tag_permadeath%5B%5D={0}&tag_difficulty%5B%5D={1}&tag_campaign%5B%5D=2&tag_game%5B%5D=699172&page={2}'.format(
-            tag_mode[mode], tag_diff[diff], i)
-        print(URL)
-        page = requests.get(URL)
-        sleep(randint(1, 3))
-        soup = BeautifulSoup(page.content, "html.parser")
-        char_table = soup.find(id="characters")
-        my_table = char_table.find_all(style="color:#FF0000")
-        for tag in my_table:
-            deaths_list.append(tag.get_text())
-    return deaths_list
-
-
-def count_classes(mode='roguelike', diff='normal', status='win'):
+    tag_status = {'win': 'tag_winner=winner',
+                  'dead': 'tag_dead=dead'}
+    # color tags to exclude wins from dead characters
     if status == 'win':
-        characters_list = get_winners(mode, diff)
+        status_code = 'color:#00FF00'
     elif status == 'dead':
-        characters_list = get_deaths(mode, diff)
+        status_code = 'color:#FF0000'
+    else:
+        return 'Wrong status'
+    next_page_exists = 'Yes'
+    page_num = 0
+    while next_page_exists == 'Yes':
+        URL = 'https://te4.org/characters-vault?tag_name=&tag_level_min={0}&tag_level_max=&{1}&tag_permadeath%5B%5D={2}&tag_difficulty%5B%5D={3}&tag_campaign%5B%5D=2&tag_game%5B%5D=699172&page={4}'.format(
+            min_lvl, tag_status[status], tag_mode[mode], tag_diff[diff], page_num)
+        print(URL)  # for debugging
+        page = requests.get(URL)
+        sleep(randint(1, 3))
+        soup = BeautifulSoup(page.content, 'html.parser')
+        char_table = soup.find(id='characters')
+        next_page_link = soup.find(title='Go to next page')
+        if next_page_link is None:
+            next_page_exists = 'No'
+        else:
+            page_num += 1
+        my_table = char_table.find_all(style=status_code)
+        for tag in my_table:
+            characters_list.append(tag.get_text())
+    return characters_list
+
+
+def count_classes(mode: str = 'roguelike', diff: str = 'normal', min_lvl: int = 15, status: str = 'win'):
+    if status == 'win' or status == 'dead':
+        characters_list = get_characters(mode, diff, min_lvl, status)
     else:
         return 'Wrong status'
     class_stats = {'Berserker': 0, 'Bulwark': 0, 'Archer': 0, 'Arcane Blade': 0, 'Brawler': 0, 'Rogue': 0,
@@ -96,3 +86,6 @@ def count_classes(mode='roguelike', diff='normal', status='win'):
     df = pd.DataFrame.from_dict(class_stats, orient='index')
     df.to_excel('characters_{0}_{1}_{2}.xlsx'.format(mode, diff, status), header=True)
     return df
+
+
+print(count_classes('adventure', 'insane', 15, 'dead'))
